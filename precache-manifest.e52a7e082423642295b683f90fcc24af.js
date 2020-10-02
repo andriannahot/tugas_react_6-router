55b068f8d99c37474b443d9901e7eafbe707da6b
andrian <64577548+andriannahot@users.noreply.github.com>
2020-10-02 09:27:16 +0700

    Add files via upload

---
+++ precache-manifest.e52a7e082423642295b683f90fcc24af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d05cad4522a932d2e1def813a8fc431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ad5dc3a86cde4543028",
+    "url": "/static/css/main.5778026f.chunk.css"
+  },
+  {
+    "revision": "937ff29c80b861d72517",
+    "url": "/static/js/2.4ca1523d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4ca1523d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad5dc3a86cde4543028",
+    "url": "/static/js/main.ce90c617.chunk.js"
+  },
+  {
+    "revision": "22bd10b89bd58de2b0e5",
+    "url": "/static/js/runtime-main.7aee4269.js"
+  }
+]);
